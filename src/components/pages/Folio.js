668b960821631817ec75103ebdd33e9a4775ca63
Will Yazdani <yazd0006@gmail.com>
2022-05-15 22:45:02 -0500

    added links to projects

--- src/components/pages/Folio.js
+++ src/components/pages/Folio.js
@@ -9,13 +9,16 @@ export default function Folio() {
   const [isOpen1, setIsOpen1] = useState(false)
   const [isOpen2, setIsOpen2] = useState(false)
   const [isOpen3, setIsOpen3] = useState(false)
+  const [isOpen4, setIsOpen4] = useState(false)
+  const [isOpen5, setIsOpen5] = useState(false)
+  const [isOpen6, setIsOpen6] = useState(false)
   return (
     <div id="wrapper">
       <div id="projects">
         <button onClick={() => setIsOpen1(true)}>
           DATELIME
         </button>
-          <Modal id="modal1" open={isOpen1} onClose={() => setIsOpen1(false)}>
+          <Modal open={isOpen1} onClose={() => setIsOpen1(false)}>
            <h1>DateLime</h1>
            <img src={datelime} alt="datelime" id="datelime" />
            <p>Role: Front End Developer
@@ -24,24 +27,46 @@ export default function Folio() {
             <br></br>
             This is an app that pairs movies and recipes based on the user!
            </p>
+           <a href="https://github.com/WillYazdani/Seven">GITHUB</a>
+           <a href="https://sonarie.github.io/Seven/">LIVE</a>
           </Modal>
         <button onClick={() => setIsOpen2(true)}>
         FULLSTACK JACK
         </button>
-          <Modal id="modal2" open={isOpen2} onClose={() => setIsOpen2(false)}>
+          <Modal open={isOpen2} onClose={() => setIsOpen2(false)}>
           <h1>FullStack Jack</h1>
            <img src={fsj} alt="fsj" id="fsj" />
            <p>Role: Front End Developer
             <br></br>
-            Technologies: HTML, CSS, JavaScript, Handlebars, jQuery, Express, Node.js, MySQL
+            Technologies: HTML, CSS, Bootstrap, JavaScript, Handlebars, jQuery, Express, Node.js, MySQL
             <br></br>
             Play Blackjack and get a high score!
            </p>
+           <a href="https://github.com/WillYazdani/full_stack_jack">GITHUB</a>
+           <a href="https://young-atoll-21457.herokuapp.com/">LIVE</a>
           </Modal>
         <button onClick={() => setIsOpen3(true)}>
         TBD
         </button>
-          <Modal id="modal3" open={isOpen3} onClose={() => setIsOpen3(false)}>
+          <Modal open={isOpen3} onClose={() => setIsOpen3(false)}>
+           TBD
+          </Modal>
+        <button onClick={() => setIsOpen3(true)}>
+        TBD
+        </button>
+          <Modal open={isOpen4} onClose={() => setIsOpen4(false)}>
+           TBD
+          </Modal>
+        <button onClick={() => setIsOpen3(true)}>
+        TBD
+        </button>
+          <Modal open={isOpen5} onClose={() => setIsOpen5(false)}>
+           TBD
+          </Modal>
+        <button onClick={() => setIsOpen3(true)}>
+        TBD
+        </button>
+          <Modal open={isOpen6} onClose={() => setIsOpen6(false)}>
            TBD
           </Modal>
       </div>
