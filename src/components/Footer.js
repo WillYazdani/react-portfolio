d19411fcc28a9c4fcf45242b8e0cfac8b086efb1
Will Yazdani <yazd0006@gmail.com>
2022-05-15 21:53:55 -0500

    added links to footer and added styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,10 @@ import "./CSS/footer.css"
 function footer ({currentPage, handlePageChange}) {
   return (
     <div className="footer" id="footer">
+      <a id="links" href="https://github.com/WillYazdani">GITHUB</a>
+      <a id="links" href="https://www.linkedin.com/in/will-yazdani/">LINKEDIN</a>
+      <a id="links" href="https://stackoverflow.com/users/19103672/will-yazdani?tab=profile">STACKOVERFLOW</a>
+
     </div>
 );
 }
