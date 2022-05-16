af432722950eef17f50a6e78738ca2da38f4a623
Will Yazdani <yazd0006@gmail.com>
2022-05-15 21:38:23 -0500

    added resume download and styles to contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,17 +4,19 @@ import "../CSS/contact.css";
 export default function Contact() {
   return (
     <div id="contact-wrapper">
+      <div id="contact-form">
       <h1>Contact Me</h1>
-      <form>
-        <label>Name:</label><br></br>
-        <input type="text" name="name" placeholder="Name" /><br></br>
-        <label>Email:</label><br></br>
-        <input type="email" name="email" placeholder="Email" /><br></br>
-        <label>Message:</label><br></br>
-        <textarea name="message" placeholder="Message"></textarea><br></br>
-        <button type="submit">Send</button>
-        <button>Download Resume</button>
-      </form>
+        <form>
+          <label>Name:</label><br></br>
+          <input type="text" name="name" placeholder="Name" /><br></br>
+          <label>Email:</label><br></br>
+          <input type="email" name="email" placeholder="Email" /><br></br>
+          <label>Message:</label><br></br>
+          <textarea name="message" placeholder="Message"></textarea><br></br>
+          <button type="submit">Send</button>
+          <a href="../assets/will-yazdani-resume-2022.docx" download>Download My Resume</a>
+        </form>
+      </div>
     </div>
   );
 }
