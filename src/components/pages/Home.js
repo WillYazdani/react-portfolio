5400067e38ee9cb8644d8b0eb0c7db7261ce01c1
Will Yazdani <yazd0006@gmail.com>
2022-07-30 16:33:14 -0500

    carousel markup added to about section

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,23 +2,58 @@ import React from 'react';
 import '../CSS/home.css';
 import bg from '../assets/bg.svg';
 import wy from '../assets/wy.svg';
+import left from '../assets/left.svg';
+import right from '../assets/right.svg';
+import html from '../assets/html.svg';
+import css from '../assets/css.svg';
+import js from '../assets/js.svg';
 
 export default function Home() {
     return (
         <>
             <div className="parallax-wrapper">
+                {/* HERO */}
                 <header>
                     <img src={bg} alt={bg} className='background' />
                     <img src={wy} alt={wy} className='logo' />
                     <h1 className="title">Will Yazdani</h1>
                     <h2 className="subtitle">Web Developer. Designer. Photographer.</h2>
                 </header>
+                {/* ABOUT SECTION */}
                 <section className='about'>
                     <h3>01.About Me</h3>
                     <p>Hey there, my name is Will Yazdani. I'm a front-end developer based in Minnesota. I've always been interested in tech and art, so I naturally gravitated towards web design while in college. This curiosity lead me to find out my love for coding. I'm also a photographer and I love to shoot and edit photos. Please take a look at my portfolio see some of my work.
                     </p>
                     <h3>Skills:</h3>
+                    <div className='carousel'>
+                        <button className='carousel__button carousel__button--left'>
+                            <img src={left} alt={left} className='carousel-button-left' />
+                        </button>
+                        <div className='carousel__track-container'>
+                            <ul className='carousel__track'>
+                                <li className='carousel__slide'>
+                                    <img src={html} alt={html} className='carousel__image' />
+                                </li>
+                                <li className='carousel__slide'>
+                                    <img src={css} alt={css} className='carousel__image' />
+                                </li>
+                                <li className='carousel__slide'>
+                                    <img src={js} alt={js} className='carousel__image' />
+                                </li>
+                            </ul>    
+                        </div>  
+                        <button className='carousel__button carousel__button--right'>
+                            <img src={right} alt={right} className='carousel-button-right' /> 
+                        </button>
+                        <div className='carousel__nav'>
+                            <button className='carousel__indicator current-slide' />
+                            <button className='carousel__indicator' />
+                            <button className='carousel__indicator' />
+                        </div> 
+                    </div>
+
                 </section>
+                {/* PROJECTS SECTION */}
                 <div className='project-wrapper'>
                     <section className='projects'>
                         <h3>02.Projects</h3>
@@ -48,6 +83,7 @@ export default function Home() {
                         </div>
                     </section>
                 </div> 
+                {/* CONTACT SECTION */}
                 <section className='contact-wrapper'>
                 <h3>03.Contact</h3>
                 I am currently seeking out front end development positions, either remote or in the Twin Cities area. Feel free to reach out with any offers, opportunities or if you just want to chat. - Will
