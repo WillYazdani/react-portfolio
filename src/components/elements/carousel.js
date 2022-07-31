94cd0088ceb385f8e3fc020c5e1f043d9e5a738d
Will Yazdani <yazd0006@gmail.com>
2022-07-31 13:31:12 -0500

    added framer motion package

--- src/components/elements/carousel.js
+++ src/components/elements/carousel.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useRef, useEffect, useState} from 'react';
+import {motion} from 'framer-motion';
 import '../CSS/home.css';
 import left from '../assets/left.svg';
 import right from '../assets/right.svg';
@@ -6,35 +7,39 @@ import html from '../assets/html.svg';
 import css from '../assets/css.svg';
 import js from '../assets/js.svg';
 
+
+
+
 export default function Carousel() {
     return(
         <div className='carousel-wrapper'>
-                        <div className='carousel'>
-                            <button className='carousel__button carousel__button--left'>
-                                <img src={left} alt={left} className='carousel-button-left' />
-                            </button>
-                            <div className='carousel__track-container'>
-                                <ul className='carousel__track'>
-                                    <li className='carousel__slide'>
-                                        <img src={html} alt={html} className='carousel__image' />
-                                    </li>
-                                    <li className='carousel__slide'>
-                                        <img src={css} alt={css} className='carousel__image' />
-                                    </li>
-                                    <li className='carousel__slide'>
-                                        <img src={js} alt={js} className='carousel__image' />
-                                    </li>
-                                </ul>    
-                            </div>  
-                            <button className='carousel__button carousel__button--right'>
-                                <img src={right} alt={right} className='carousel-button-right' /> 
-                            </button>
-                            <div className='carousel__nav'>
-                                <button className='carousel__indicator current-slide' />
-                                <button className='carousel__indicator' />
-                                <button className='carousel__indicator' />
-                            </div> 
-                        </div>
-                    </div>
+            {/* <motion.h1 animate={{x:550}}>hello</motion.h1> */}
+            <div className='carousel'>
+                <button className='carousel__button carousel__button--left'>
+                    <img src={left} alt={left} className='carousel-button-left' />
+                </button>
+                <div className='carousel__track-container'>
+                    <ul className='carousel__track'>
+                        <li className='carousel__slide'>
+                            <img src={html} alt={html} className='carousel__image' />
+                        </li>
+                        <li className='carousel__slide'>
+                            <img src={css} alt={css} className='carousel__image' />
+                        </li>
+                        <li className='carousel__slide'>
+                            <img src={js} alt={js} className='carousel__image' />
+                        </li>
+                    </ul>    
+                </div>  
+                <button className='carousel__button carousel__button--right'>
+                    <img src={right} alt={right} className='carousel-button-right' /> 
+                </button>
+                <div className='carousel__nav'>
+                    <button className='carousel__indicator current-slide' />
+                    <button className='carousel__indicator' />
+                    <button className='carousel__indicator' />
+                </div> 
+            </div>
+        </div>
     )
 }
