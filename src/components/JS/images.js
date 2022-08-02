df481638b4a75a79c8e18d88805cf5f186573371
Will Yazdani <yazd0006@gmail.com>
2022-08-02 08:33:46 -0500

    added new elements to skills carousel

--- src/components/JS/images.js
+++ src/components/JS/images.js
@@ -2,5 +2,8 @@ import html from '../assets/html.svg';
 import css from '../assets/css.svg';
 import js from '../assets/js.svg';
 import react from '../assets/react.svg';
+import sass from '../assets/sass.svg';
+import node from '../assets/node.svg';
+import cc from '../assets/cc.svg';
 
-export default [html, css, js, react];
+export default [html, css, js, react, sass, node, cc];
