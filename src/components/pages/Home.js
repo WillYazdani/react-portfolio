f317ae5f0242cad0f379cd055276afa584ec1e3f
Will Yazdani <yazd0006@gmail.com>
2022-08-03 17:32:06 -0500

    added project buttons

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {motion} from 'framer-motion';
 import '../CSS/home.css';
 import bg from '../assets/bg.svg';
 import wy from '../assets/wy.svg';
@@ -8,14 +9,18 @@ export default function Home() {
     return (
         <>
             <div className="parallax-wrapper">
+
                 {/* HERO */}
+
                 <header>
                     <img src={bg} alt={bg} className='background' />
                     <img src={wy} alt={wy} className='logo' />
                     <h1 className="title">Will Yazdani</h1>
                     <h2 className="subtitle">Web Developer. UX/UI Designer.</h2>
                 </header>
+
                 {/* ABOUT SECTION */}
+
                 <section className='about'>
                     <h3>01.About Me</h3>
                     <p>Hey there, my name is Will Yazdani. I'm a front-end developer based in Minnesota. I've always been interested in tech and art, so I naturally gravitated towards web design while in college. This curiosity lead me to find out my love for coding. Take a look at section 02 to see some of my work and feel free to contact me in section 03.
@@ -23,37 +28,86 @@ export default function Home() {
                     <h3>Skills:</h3>
                     <Carousel></Carousel>
                 </section>
+
                 {/* PROJECTS SECTION */}
-                <div className='project-wrapper'>
-                    <section className='projects'>
-                        <h3>02.Projects</h3>
-                        <div className='project-container'>
-                            <div className='project-card'>
-                                <h4>Parallax Gallery</h4>
-                                <p>
-                                    Parallax Gallery is a simple gallery that uses parallax scrolling to create a 3D effect.
-                                </p>
-                            </div>
-                        </div>
-                        <div className='project-container'>
-                            <div className='project-card'>
-                                <h4>FullStackJack</h4>
-                                <p>
-                                    Learn to play BlackJack with this fully functional online version.
-                                </p>
-                            </div>
-                        </div>
-                        <div className='project-container'>
-                            <div className='project-card'>
-                                <h4>Mental Health Check</h4>
-                                <p>
-                                    This app provides the user with a quick and easy way to evaluate their mental health and gives them resources to help their current situation.
-                                </p>
-                            </div>
-                        </div>
-                    </section>
-                </div> 
+                
+                <section className='projects'>
+                    <h3>02.Projects</h3>
+
+                    <motion.button
+                        className = "project-button"
+                        whileHover={{
+                            scale: 1.05,
+                            transition: {
+                                duration: 0.2,
+                                ease: "easeInOut"
+                            }
+                        }}
+                        whileTap={{
+                            scale: 0.95,
+                            transition: {
+                                duration: 0.2,
+                                ease: "easeInOut"
+                            }
+                        }}
+                        onClick={() => null}
+                    >
+                        <h4 className='project-title'>Parallax Gallery</h4>
+                        <p className='project-description'>
+                        Parallax Gallery is a simple gallery that uses parallax scrolling to create a 3D effect.
+                        </p>
+                    </motion.button>
+                    <motion.button
+                        className = "project-button"
+                        whileHover={{
+                            scale: 1.05,
+                            transition: {
+                                duration: 0.2,
+                                ease: "easeInOut"
+                            }
+                        }}
+                        whileTap={{
+                            scale: 0.95,
+                            transition: {
+                                duration: 0.2,
+                                ease: "easeInOut"
+                            }
+                        }}
+                        onClick={() => null}
+                    >
+                        <h4 className='project-title'>ReactJack</h4>
+                        <p className='project-description'>
+                        Learn to play BlackJack with this fully functional online version.
+                        </p>
+                    </motion.button>
+                    <motion.button
+                        className = "project-button"
+                        whileHover={{
+                            scale: 1.05,
+                            transition: {
+                                duration: 0.2,
+                                ease: "easeInOut"
+                            }
+                        }}
+                        whileTap={{
+                            scale: 0.95,
+                            transition: {
+                                duration: 0.2,
+                                ease: "easeInOut"
+                            }
+                        }}
+                        onClick={() => null}
+                    >
+                        <h4 className='project-title'>Mental Health Check</h4>
+                        <p className='project-description'>
+                        This app provides the user with a quick and easy way to evaluate their mental health and gives them resources to help their current situation.
+                        </p>
+                    </motion.button>
+
+                </section>
+                
                 {/* CONTACT SECTION */}
+
                 <section className='contact-wrapper'>
                 <h3>03.Contact</h3>
                 I am currently seeking out front end development positions, either remote or in the Twin Cities area. Feel free to reach out with any offers, opportunities or if you just want to chat. - Will
@@ -70,10 +124,10 @@ export default function Home() {
                         <div className='contact-card'>
                             <a id="links" href="https://www.linkedin.com/in/will-yazdani/">LINKEDIN</a><br/>
                             <a id="links" href="https://github.com/WillYazdani">GITHUB</a><br/>
-                            {/* <a id="links" href="https://leetcode.com/willyazdani/">LEETCODE</a><br/> */}
                         </div>  
                     </div>    
-                </section> 
+                </section>
+
             </div>
         </>   
     );
